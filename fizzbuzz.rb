0e37dc9dcfbf1034eed36e7f2510e7a6937cfdf1
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-09 04:17:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,8 @@ def fizzbuzz(int)
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
-  elsif int % 3 && 5 == 0
-    "Fizzbuzz"
-  else 
+  elsif int % 3 && 5     "Fizzbuzz"
+  else
     "nil"
   end
 end
